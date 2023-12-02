b3e5765a5e7494566089d1ac24cb0e0a65041d8e
acasillas1407 <acasillas1407@gmail.com>
2023-12-02 16:43:26 -0700

    work

--- Our_Versh.R
+++ Our_Versh.R
@@ -6,6 +6,12 @@ library(cowplot)
 library(tidyverse)
 library(GetBCBData)
 library(forecast)
+library(knitr)
+library(kableExtra)
+library(writexl)
+library(FinTS)
+library(texreg)
+library(rugarch)
 
 ###################################################
 first_date <- '2003-12-01' 
@@ -18,8 +24,8 @@ df$Close = df$Close / 1000 # IN USD per Kilogram
 
 # select columns and calculated log_ret and arim_ret
 df_prices <- df  %>%
-  mutate(log_ret = log(Close/dplyr::lag(Close) ),
-         arim_ret = Close/dplyr::lag(Close) - 1,
+  mutate(log_ret = log(Close/dplyr::lag(Close) ),   # Log Returns
+         arim_ret = Close/dplyr::lag(Close) - 1,  # Arithmetic Returns
          series_name = series_name) %>%
   na.omit() # remove all NA values
 
@@ -49,8 +55,8 @@ series_name <- df_prices$series_name[1]
 
 df_prices$Date <- as.Date(df_prices$Date, format = "%m/%d/%Y")
 
-# get inflation data - using the Broad National Consumer Index 
-df_inflation <- gbcbd_get_series(id = 433, first.date = min(df_prices$Date), 
+# get inflation data - using the United States CPI
+df_inflation <- gbcbd_get_series(id = 3794, first.date = min(df_prices$Date), 
                                  last.date = max(df_prices$Date)) %>% 
   mutate(inf_index  = cumprod(1+value/100))
 
@@ -99,15 +105,125 @@ p2 <- ggplot(df_prices,
   labs(size = 'Absolute Price Variation') # + 
 scale_color_brewer(palette = 'BrBG')
 
+
 p2
+
 
 pb <- plot_grid(p1, p2, nrow = 2, 
                labels = 'AUTO')
 
 pb
 
+
 p <- ggAcf(x = df_prices$log_ret, lag.max = 10) +
   labs(title = paste0('Autocorrelogram for the Log Returns of ', series_name)) +
   theme_bw(base_family = "TT Times New Roman")
 
 p
+
+##################################################################################
+
+max_lag <- 5
+my_html_file <- 'tab03-Arch_Test.html'
+my_xlsx_file <- 'tab03-Arch_Test.xlsx'
+
+if (!dir.exists(dirname(my_html_file))) dir.create(dirname(my_html_file))
+
+tab_out <- do_arch_test(x = df_prices$log_ret, max_lag = max_lag)
+
+tab_out
+
+# remove attributes of table so it can be correctly parsed in html
+tab_out <- as.data.frame(
+  lapply(tab_out, function(x) { attributes(x) <- NULL; x })
+)
+str(tab_out)
+
+rownames(tab_out) <- NULL
+
+# save table in html
+my_tbl <- knitr::kable(tab_out, format = 'html' ) %>%
+  kable_styling(bootstrap_options = c("striped"), 
+                full_width = FALSE ) 
+
+my_tbl
+
+cat(my_tbl, file = my_html_file)  
+
+# write to excel
+write_xlsx(x = tab_out, path = my_xlsx_file)
+########################################################################
+
+# OPTIONS
+ar_lag <- 0 # lag used for ar term in mean equation (0 in paper)
+ma_lag <- 0 # lag used for ma term in mean equation (0 in paper)
+arch_lag <- 1 # lag in arch effect (1 in paper)
+garch_lag <- 1 # lag in garch effect (1 in paper)
+models_to_estimate <- c('sGARCH', 'eGARCH', 'gjrGARCH') # see rugarch manual for more
+distribution_to_estimate <- 'norm' # distribution used in all models
+my_html_file <- 'tab04-estimation_garch.html' # where to save html file?
+
+# close all opened windows
+graphics.off()
+
+df_grid <- expand_grid(ar_lag,
+                       ma_lag,
+                       arch_lag,
+                       garch_lag,
+                       models_to_estimate,
+                       distribution_to_estimate)
+
+
+estimate_garch <- function(ar_lag,
+                           ma_lag,
+                           arch_lag,
+                           garch_lag,
+                           models_to_estimate,
+                           distribution_to_estimate) {
+  
+  message('Estimating ARMA(',ar_lag,',', ma_lag, ')', '-',
+          models_to_estimate, '(', arch_lag, ',', garch_lag, ') ', 
+          'dist = ', distribution_to_estimate)
+  
+  # estimate model
+  my_spec <- ugarchspec(variance.model = list(model = models_to_estimate,
+                                              garchOrder = c(arch_lag, 
+                                                             garch_lag)),
+                        mean.model = list(armaOrder = c(ar_lag,
+                                                        ma_lag)), 
+                        distribution.model = distribution_to_estimate)
+  
+  my_garch <- ugarchfit(spec = my_spec, data = df_prices$log_ret)
+  
+  return(my_garch)
+}
+
+# estimate all models
+l_args <- as.list(df_grid)
+l_models <- pmap(.l = l_args, .f = estimate_garch)
+
+# make sure dir "tabs" exists
+if (!dir.exists('tabs')) dir.create('tabs')
+
+# reformat models for texreg
+l_models <- map(l_models, extract.rugarch, include.rsquared = FALSE)
+
+# write custom row
+custom_row <- list('Variance Model' = df_grid$models_to_estimate,
+                   'Distribution' = df_grid$distribution_to_estimate)
+custom_names <- paste0('Model ', 1:length(l_models))
+
+# save to html
+htmlreg(l_models, 
+        file = my_html_file, 
+        custom.gof.rows = custom_row,
+        custom.model.names = custom_names, 
+        digits = 3)
+
+# print to screen
+screenreg(l_models,
+          custom.gof.rows = custom_row,
+          custom.model.names = custom_names, 
+          digits = 3)
+#########################################################
+
