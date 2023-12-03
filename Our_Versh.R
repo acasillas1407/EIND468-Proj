d80dea9ae991a07e811e981abe5c8fec064f4cb2
acasillas1407 <acasillas1407@gmail.com>
2023-12-02 19:13:41 -0700

    Update Our_Versh.R

--- Our_Versh.R
+++ Our_Versh.R
@@ -12,6 +12,8 @@ library(writexl)
 library(FinTS)
 library(texreg)
 library(rugarch)
+library(purrr)
+library(ggtext)
 
 ###################################################
 first_date <- '2003-12-01' 
@@ -28,6 +30,9 @@ df_prices <- df  %>%
          arim_ret = Close/dplyr::lag(Close) - 1,  # Arithmetic Returns
          series_name = series_name) %>%
   na.omit() # remove all NA values
+
+colnames(df_prices)[colnames(df_prices) == "Date"] <- "ref.date"
+colnames(df_prices)[colnames(df_prices) == "Close"] <- "price.adjusted"
 
 # save data into file
 df_out <- 'GARCH-Data.rds'
@@ -49,20 +54,18 @@ if (!dir.exists('figs')) dir.create('figs')
 # source functions 
 source('garch_fcts.R')
 
-# get price data
-df_prices <- read_rds('data/GARCH-Data.rds')
-series_name <- df_prices$series_name[1]
 
-df_prices$Date <- as.Date(df_prices$Date, format = "%m/%d/%Y")
+df_prices$ref.date <- as.Date(df_prices$ref.date, format = "%m/%d/%Y")
 
-# get inflation data - using the United States CPI
-df_inflation <- gbcbd_get_series(id = 3794, first.date = min(df_prices$Date), 
-                                 last.date = max(df_prices$Date)) %>% 
+
+# get inflation data - using the United States CPI http://www.bcb.gov.br/?sgs
+df_inflation <- gbcbd_get_series(id = 3794, first.date = min(df_prices$ref.date), 
+                                 last.date = max(df_prices$ref.date)) %>% 
   mutate(inf_index  = cumprod(1+value/100))
 
-total_cop_ret <- last(df_prices$Close)/first(df_prices$Close)-1
+total_cop_ret <- last(df_prices$price.adjusted)/first(df_prices$price.adjusted)-1
 total_inflation <- last(df_inflation$inf_index)/first(df_inflation$inf_index) - 1 
-n_years <- as.numeric(max(df_prices$Date) - min(df_prices$Date))/365
+n_years <- as.numeric(max(df_prices$ref.date) - min(df_prices$ref.date))/365
 ret_cop_year = (1+total_cop_ret)^(1/n_years) - 1
 ret_inflation_year = (1+total_inflation)^(1/n_years) - 1
 
@@ -70,7 +73,7 @@ real_ret_cop <- (1+total_cop_ret)/(1+total_inflation) - 1
 real_ret_cop_year <- (1+ret_cop_year)/(1+ret_inflation_year) - 1
 
 # create first plot
-p1 <- ggplot(df_prices, aes(x = Date, y = Close)) + 
+p1 <- ggplot(df_prices, aes(x = ref.date, y = price.adjusted)) + 
   geom_line() + 
   labs(title = paste0('Prices of ', series_name),
        subtitle = paste0('Total nominal arithmetic return equals to ', 
@@ -93,7 +96,7 @@ largest_tab <- df_prices %>%
 
 # create second plot
 p2 <- ggplot(df_prices, 
-             aes(x = Date, y = log_ret)) + 
+             aes(x = ref.date, y = log_ret)) + 
   geom_line() + 
   labs(title = paste0('Nominal Daily Log Returns of ', series_name),
        subtitle = paste0('Red circles represent the largest ', n_largest, 
@@ -102,7 +105,7 @@ p2 <- ggplot(df_prices,
        y = 'Log Returns',
        caption = 'Data from Business Insider') + 
   theme_bw(base_family = "TT Times New Roman") +
-  geom_point(data = largest_tab, aes(x = Date, y = log_ret), 
+  geom_point(data = largest_tab, aes(x = ref.date, y = log_ret), 
              size = 3, color = 'red'  ) +
   scale_y_continuous(labels = scales::percent) + 
   labs(size = 'Absolute Price Variation') # + 
@@ -236,3 +239,143 @@ screenreg(l_models,
           digits = 3)
 #########################################################
 
+max_lag_AR <- 1 # used 1 in paper
+max_lag_MA <- 1 # used 1 in paper
+max_lag_ARCH <- 2 # used 2 in paper
+max_lag_GARCH <- 1 # used 1 in paper
+dist_to_use <- c('norm', 'std') # see rugarch::ugarchspec help for more
+
+graphics.off()
+
+out <- find_best_arch_model(x = df_prices$log_ret, 
+                            type_models = models_to_estimate,
+                            dist_to_use = dist_to_use,
+                            max_lag_AR = max_lag_AR,
+                            max_lag_MA = max_lag_MA,
+                            max_lag_ARCH = max_lag_ARCH,
+                            max_lag_GARCH = max_lag_GARCH)
+
+tab_out <- out$tab_out
+
+# pivot table to long format (better for plotting)
+df_long <- tidyr::pivot_longer(data = tab_out %>%
+                                 select(model_name,
+                                        type_model,
+                                        type_dist,
+                                        AIC, BIC),  cols = c('AIC', 'BIC'))
+
+models_names <- unique(df_long$model_name)
+best_models <- c(tab_out$model_name[which.min(tab_out$AIC)],
+                 tab_out$model_name[which.min(tab_out$BIC)])
+
+# figure out where is the best model
+df_long <- df_long %>%
+  mutate(order_model = if_else(model_name %in% best_models, 'Best Model', 'Not Best Model') ) %>%
+  na.omit()
+
+# make table with best models
+df_best_models <- df_long %>%
+  group_by(name) %>%
+  summarise(model_name = model_name[which.min(value)],
+            value = value[which.min(value)],
+            type_model = type_model[which.min(value)])
+
+# plot results
+p3 <- ggplot(df_long %>%
+               arrange(type_model), 
+             aes(x = reorder(model_name, 
+                             order(type_model)),
+                 y = value, 
+                 shape = type_dist,
+                 color = type_model)) + 
+  geom_point(size = 3.5, alpha = 0.65) + 
+  coord_flip() + 
+  theme_bw(base_family = "TT Times New Roman") + 
+  facet_wrap(~name, scales = 'free_x') + 
+  geom_point(data = df_best_models, mapping = aes(x = reorder(model_name, 
+                                                              order(type_model)),
+                                                  y = value), 
+             color = 'blue', size = 5, shape = 8) +
+  labs(title = 'Selecting Garch Models by Fitness Criteria', 
+       subtitle = 'The best model is the one with lowest AIC or BIC (with star)',
+       x = '',
+       y = 'Value of Fitness Criteria',
+       shape = 'Type of Dist.',
+       color = 'Type of Model') + 
+  theme(legend.position = "right")
+
+ p3 ; ggsave('figs/fig04_best_garch.png')
+ 
+ p3
+
+
+# estimate best garch model by BIC (used in next section)
+best_spec = ugarchspec(variance.model = list(model =  out$best_bic$type_model, 
+                                             garchOrder = c(out$best_bic$lag_arch,
+                                                            out$best_bic$lag_garch)),
+                       mean.model = list(armaOrder = c(out$best_bic$lag_ar, 
+                                                       out$best_bic$lag_ma)),
+                       distribution = 'std')
+
+my_best_garch <- ugarchfit(spec = best_spec, 
+                           data = df_prices$log_ret)
+
+my_best_garch
+
+write_rds(my_best_garch, 'data/garch_model.rds')
+########################################################################
+
+my_garch = my_best_garch
+
+set.seed(8008135) 
+n_sim <- 5040
+
+n_days_ahead <- 2*365 # Number of days ahead to simulate (10*365 in paper)
+
+graphics.off()
+
+
+
+df_sim <- do_sim(n_sim = n_sim, 
+                 n_t = n_days_ahead, 
+                 my_garch, 
+                 df_prices = df_prices)
+
+glimpse(df_sim )
+
+# calculate probabilities of reaching peak value
+tab_prob <- df_sim %>%
+  group_by(ref_date) %>%
+  summarise(prob = mean(sim_price > max(df_prices$price.adjusted)))
+
+n_years_back <- 4
+df_prices_temp <- df_prices %>%
+  dplyr::filter(ref.date > max(ref.date) - n_years_back*365)
+
+my_garch_name <- toupper(as.character(my_garch@model$modeldesc$vmodel))
+
+p4 <- ggplot() + 
+  geom_line(data = df_prices_temp, 
+            aes(x = ref.date, y = price.adjusted), color = 'black', size = 0.75)  + 
+  geom_line(data = df_sim, 
+            aes(x = ref_date, 
+                y = sim_price, 
+                group = i_sim),
+            color = 'gray', 
+            size = 0.25,
+            alpha = 0.015) + 
+  theme_bw(base_family = "TT Times New Roman") + 
+  geom_hline(yintercept = max(df_prices_temp$price.adjusted)) + 
+  labs(title = paste0('Price Projections of ', series_name),
+       subtitle = paste0('Total of ', n_sim, ' simulations based on a ',
+                         my_garch_name, 
+                         ' model selected by BIC'),
+       caption = 'Data from Yahoo Finance',
+       x = '',
+       y = 'Value') + 
+  ylim(c(0.75*min(df_prices_temp$price.adjusted), 
+         1.25*max(df_prices_temp$price.adjusted))) + 
+  xlim(c(max(df_prices_temp$ref.date) - n_years_back*365,
+         max(df_prices_temp$ref.date) + 2*365) )
+
+p1 
